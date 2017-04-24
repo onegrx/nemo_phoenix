26c9c40316e46d69aba2670c4796a22511b155cf
bartoszszafran <bszafpl@gmail.com>
2017-04-24 18:49:02 +0200

    Add test suite for Config module

---
+++ test/nemo/config_test.exs
@@ -0,0 +1,32 @@
+defmodule Nemo.ConfigTest do
+  use ExUnit.Case
+
+  alias Tide.Config
+
+  setup_all do
+    vars = [key1: :val1,
+            key2: [nested_key1: :nested_val1,
+                   nested_key2: :nested_val2]]
+    vars
+    |> Enum.each(fn ({key, val}) ->
+      Application.put_env(:nemo, key, val)
+    end
+    )
+  end
+
+  test "Desired value could be retrived with module" do
+    assert :val1 = Config.get :key1
+    assert :val1 = Config.get :key1, :default
+    assert :nested_val2 = Config.get [:key2, :nested_key2]
+    assert :nested_val2 = Config.get [:key2, :nested_key2], :default
+  end
+
+  test ":undefined is returned for undefined variable" do
+    assert :undefined = Config.get :non_existing
+  end
+
+  test "default value is returned for undefined variable" do
+    assert :default = Config.get :non_existing, :default
+  end
+
+end
