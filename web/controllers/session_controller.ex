5244f463d979fac5c526d49de0907650551f2e3f
onegrx <onegrx@gmail.com>
2017-05-04 01:17:12 +0200

    Sumbit login form and authenticate user

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -4,4 +4,19 @@ defmodule Nemo.SessionController do
   def new(conn, _params) do
     render conn, "new.html"
   end
+
+  def create(conn, %{"session" => session_params}) do
+    case Nemo.Session.login(session_params, Nemo.Repo) do
+      {:ok, user} ->
+        conn
+        |> put_session(:current_user, user.id)
+        |> put_flash(:info, "Logged in")
+        |> redirect(to: "/")
+      :error ->
+        conn
+        |> put_flash(:info, "Wrong email or password")
+        |> render("new.html")
+    end
+  end
+
 end
