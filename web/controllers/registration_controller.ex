9083ec999d12596a06df91318dfd2616f647adda
onegrx <onegrx@gmail.com>
2017-05-04 00:13:42 +0200

    Register user with email and password

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -6,4 +6,20 @@ defmodule Nemo.RegistrationController do
     changeset = User.changeset(%User{})
     render conn, changeset: changeset
   end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+
+    case Nemo.Registration.create(changeset, Nemo.Repo) do
+      {:ok, _changeset} ->
+        conn
+        |> put_flash(:info, "Your account was created")
+        |> redirect(to: "/")
+      {:error, changeset} ->
+        conn
+        |> put_flash(:info, "Unable to create account")
+        |> render("new.html", changeset: changeset)
+    end
+  end
+
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,8 @@
 defmodule Nemo.User do
   use Nemo.Web, :model
 
+  @required_fields ~w(email password)
+
   schema "users" do
     field :email, :string
     field :token, :string
@@ -21,7 +23,16 @@ defmodule Nemo.User do
   """
   def changeset(struct, params \\ %{}) do
     struct
+<<<<<<< HEAD
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
+=======
+    |> cast(params, [:id, :email, :token, :crypted_password, :password])
+    # |> validate_required([:id, :email, :crypted_password]) TODO fix
+    |> unique_constraint(:email)
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:password, min: 5)
+>>>>>>> dd16a08... Register user with email and password
   end
+
 end
