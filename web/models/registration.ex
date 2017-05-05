a79b7007420fb805526cf492751a35068398b7c1
onegrx <onegrx@gmail.com>
2017-05-05 23:23:56 +0200

    Generate user token

--- web/models/registration.ex
+++ web/models/registration.ex
@@ -4,10 +4,18 @@ defmodule Nemo.Registration do
   def create(changeset, repo) do
     changeset
     |> put_change(:crypted_password, hashed_password(changeset.params["password"]))
+    |> put_change(:token, generate_unique_token(changeset.params["email"])) #TODO username
     |> repo.insert()
   end
 
   defp hashed_password(password) do
     Comeonin.Bcrypt.hashpwsalt(password)
+  end
+
+  defp generate_unique_token(username, length \\ 7) do
+    token = :crypto.strong_rand_bytes(length)
+      |> Base.url_encode64
+      |> binary_part(0, length)
+    username <> "-" <> token
   end
 end
