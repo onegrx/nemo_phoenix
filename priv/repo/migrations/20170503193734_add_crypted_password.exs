0be17eb97500b00310a53df20eca3f25d923549f
onegrx <onegrx@gmail.com>
2017-05-05 23:11:49 +0200

    Prevent email duplications

--- priv/repo/migrations/20170503193734_add_crypted_password.exs
+++ priv/repo/migrations/20170503193734_add_crypted_password.exs
@@ -1,10 +1,14 @@
-defmodule Nemo.Repo.Migrations.AddCryptedPassword do
+defmodule Nemo.Repo.Migrations.AddCryptedPasswordUsername do
   use Ecto.Migration
 
   def change do
     alter table(:users) do
       add :crypted_password, :string
+      add :username, :string
     end
-    
+
+    create unique_index(:users, [:email])
+    create unique_index(:users, [:username])
+
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -4,7 +4,8 @@ defmodule Nemo.User do
   @required_fields ~w(email password)
 
   schema "users" do
-    field :email, :string
+    field :email, :string, unique: true
+    field :username, :string, unique: true
     field :token, :string
     field :password, :string
     field :username, :string
@@ -26,6 +27,7 @@ defmodule Nemo.User do
     |> cast(params, @required_fields)
     |> validate_required(@required_fields)
     |> unique_constraint(:email)
+    |> unique_constraint(:username)
     |> validate_format(:email, ~r/@/)
     |> validate_length(:password, min: 5)
   end
