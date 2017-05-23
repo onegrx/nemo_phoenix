5f2b74244fd3f746ade9f554632c27a8262a2bd1
onegrx <onegrx@gmail.com>
2017-05-23 17:15:51 +0200

    Send GET to /api from browser

--- browsers/chrome/nemo.js
+++ browsers/chrome/nemo.js
@@ -1,10 +1,24 @@
 function getword(info,tab) {
 
-    console.log("Searching for " + info.selectionText);
+    var word = info.selectionText;
+    console.log("Searching for " + word);
 
     chrome.tabs.create({
-        url: "https://www.diki.pl/slownik-angielskiego/?q=" + info.selectionText,
+        url: "https://www.diki.pl/slownik-angielskiego/?q=" + word,
     })
+
+    var xhr = new XMLHttpRequest();
+
+    var token = "bart-CqB38Dv"
+    var translated = "sth"
+
+    var url = "http://localhost:4000/api/stats?token=" + token
+      + "&word=" + word
+      + "&translated=" + translated
+
+    xhr.open("GET", url, false);
+    xhr.send();
+    console.log("Response: " + xhr.responseText);
 
 
 }
