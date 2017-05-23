1b2f4e76e55262db5f5349532034416ea8ded40a
onegrx <onegrx@gmail.com>
2017-05-24 00:27:03 +0200

    Show curent token and inform when new saved

--- browsers/chrome/storage.js
+++ browsers/chrome/storage.js
@@ -2,6 +2,12 @@ var bkg = chrome.extension.getBackgroundPage();
 
 document.addEventListener('DOMContentLoaded', function() {
 
+
+  if(localStorage["token"]) {
+    var x = document.getElementById("token");
+    if(x) x.value = localStorage["token"];
+  }
+
   var s = document.getElementById("submit")
   if(s) {
     s.addEventListener("click", handler);
@@ -12,4 +18,5 @@ function handler() {
   var token = document.getElementById("token").value;
   localStorage["token"] = token;
   bkg.console.log("Saved token " + token);
+  alert("Zapisano token: " + token);
 }
