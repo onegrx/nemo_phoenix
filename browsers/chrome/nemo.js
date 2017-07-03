167100e2162287067b48c91285f687b3f55b9fba
onegrx <onegrx@gmail.com>
2017-07-03 06:55:42 +0200

    Migrate to from Transltr to Yandex

--- browsers/chrome/nemo.js
+++ browsers/chrome/nemo.js
@@ -1,3 +1,5 @@
+var API_KEY = "trnsl.1.1.20170703T043733Z.f447f002f919a8c9.311eddfe75333dc54d75bdf8488e0da2de29d0e3"
+
 function getword(info,tab) {
 
     var xhr = new XMLHttpRequest();
@@ -12,13 +14,14 @@ function getword(info,tab) {
         url: "https://www.diki.pl/slownik-angielskiego/?q=" + word,
     })
 
+    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + API_KEY + "&text=" + word + "&lang=en-pl"
+
     var client = new XMLHttpRequest();
-    client.open("GET", "http://www.transltr.org/api/translate?text=" + word + "&to=pl&from=en");
-    client.setRequestHeader("Accept", "application/json");
+    client.open("GET", "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + API_KEY + "&text=" + word + "&lang=en-pl");
     client.onload = function(e) {
       console.log("Response: " + client.responseText);
       var jsonResponse = JSON.parse(client.responseText);
-      translated = jsonResponse["translationText"].toLowerCase();
+      translated = jsonResponse["text"][0].toLowerCase();
       console.log(translated);
 
       var url = "http://localhost:4000/api/stats?token=" + token
